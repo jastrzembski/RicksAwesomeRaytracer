cae63372c15917cea800672fe79cf2d46cdca27b
jastrzembski <bj306051@student.polsl.pl>
2023-06-19 08:49:58 +0200

    Sphere intersection debug

--- src/Sphere.cpp
+++ src/Sphere.cpp
@@ -14,12 +14,12 @@ bool Sphere::intersect(const Ray &ray, RayHit &ray_hit) const {
 
     if (t0 < 0) {
         t0 = t1;
-        if (t0 < 0) return false;
+        if (t0 < 0.001 || t0 > 10000) return false;
     }
 
     double distance = t0;
 
-    if (ray_hit.distance > 0 && (!ray_hit.hit || ray_hit.distance < distance)) {
+    if (!ray_hit.hit || ray_hit.distance < distance) {
         ray_hit.hit = true;
         ray_hit.distance = distance;
         ray_hit.intersection_point = ray.origin + ray.direction * distance;
